619469bee66417950df92b05743d6a0b49376eee
cognivore <66186054+cognivore@users.noreply.github.com>
2020-06-24 16:51:50 +0300

    Problem: we don't have directory layout for visualiser
    Solution:
    
    Using https://github.com/spielrs/yew-parcel-template, with
    
    ```
    sudo apt-get install pkg-config libssl-dev
    cargo install wasm-pack
    ```
    
    Initialize full stack Rust framework called `yew` in `visualiser` subdirectory.
    
    Next up: check if it works with filesystem-based module imports.

---
+++ visualiser/crate/src/app.rs
@@ -0,0 +1,74 @@
+use crate::pages::{About, Home};
+use yew::prelude::*;
+use yew_router::{prelude::*, route::Route, switch::Permissive, Switch};
+use yew_styles::{
+    navbar::{
+        navbar_component::{Fixed, Navbar},
+        navbar_container::NavbarContainer,
+    },
+    styles::{Palette, Style},
+};
+
+pub struct App;
+
+#[derive(Switch, Debug, Clone)]
+pub enum AppRouter {
+    #[to = "/!"]
+    RootPath,
+    #[to = "/about!"]
+    AboutPath,
+    #[to = "/page-not-found"]
+    PageNotFound(Permissive<String>),
+}
+
+impl Component for App {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        App {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <Navbar
+                    navbar_type=Palette::Info
+                    navbar_style=Style::Outline
+                    fixed=Fixed::Top
+                    branch=html!{<img src="./yew.svg"></img>}
+                >
+                    <NavbarContainer>
+                            <RouterAnchor<AppRouter> classes="navbar-item" route=AppRouter::RootPath>{"Home"}</RouterAnchor<AppRouter>>
+                            <RouterAnchor<AppRouter> classes="navbar-item" route=AppRouter::AboutPath>{"About"}</RouterAnchor<AppRouter>>
+                    </NavbarContainer>
+                </Navbar>
+                <Router<AppRouter, ()>
+                    render = Router::render(|switch: AppRouter | {
+                        match switch {
+                            AppRouter::RootPath => html!{
+                                <Home/>
+                            },
+                            AppRouter::AboutPath => html!{
+                                <About/>
+                            },
+                            AppRouter::PageNotFound(Permissive(None)) => html!{"Page not found"},
+                            AppRouter::PageNotFound(Permissive(Some(missed_route))) => html!{format!("Page '{}' not found", missed_route)}
+                        }
+                    } )
+                    redirect = Router::redirect(|route: Route<()>| {
+                        AppRouter::PageNotFound(Permissive(Some(route.route)))
+                    })
+                />
+            </div>
+        }
+    }
+}
